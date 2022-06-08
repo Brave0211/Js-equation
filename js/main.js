5a7ac05198012af70f1ebdb9320dd7dcad33784b
Brave0211 <abduqodirovjasurbek11@gmail.com>
2022-06-08 10:13:45 +0500

    Js add

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var user = document.querySelector(".answer")
+
+var userAnswer = prompt("Qancha puliz bor?")
+
+if(userAnswer >= "9666000 so'm"){
+   user.textContent = "Oq yo'l, Alisher"
+}else{
+   user.textContent = "Alisher, ozgina sabr qilish kerak bo’lar ekan."
+}
